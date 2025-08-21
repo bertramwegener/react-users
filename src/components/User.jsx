1ce52dbbe36c48d5f0a81fa292755b0e9a830441
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-08-21 14:47:09 +0200

    detaljer knap

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 export default function User({ user }) {
+  const [likes, setLikes] = useState(0);
+  const [showDetails, setShowDetails] = useState(true);
+
   return (
     <div className="user-card">
       <img src={user.image} />
       <h2>{user.name}</h2>
+
+{
+  showDetails && <div>{
+    <div>
       <p>Mail: {user.mail}</p>
+      <p>Likes: {likes}</p>
+      <button onClick={() => setLikes(likes + 1)}>Like</button>
+      <button onClick={() => setLikes(0)}>Reset likes</button>
+    </div>}
+  </div>
+}
+  
+      <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? "Skjul" : "Vis"} detaljer</button>
     </div>
   );
 }
