bc216f737147ec7028cae0674a1bd0394702ef8e
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-08-21 15:02:22 +0200

    layout update

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -8,17 +8,18 @@ export default function User({ user }) {
       <img src={user.image} />
       <h2>{user.name}</h2>
 
-{
-  showDetails && <div>{
-    <>
-      <p>Mail: {user.mail}</p>
-      <p>Likes: {likes}</p>
+      {
+        showDetails && <div>{
+          <>
+            <p>Mail: {user.mail}</p>
+            <p>Likes: {likes}</p>
+      
+            <button onClick={() => setLikes(0)}>Reset likes</button>
+          </>}
+        </div>
+      }
+      
       <button onClick={() => setLikes(likes + 1)}>Like</button>
-      <button onClick={() => setLikes(0)}>Reset likes</button>
-    </>}
-  </div>
-}
-  
       <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? "Skjul" : "Vis"} detaljer</button>
     </div>
   );
